dc5125bb5bf29cba5ac0a1578dbf2d87bd70e114
corgifighter <stevenh1998@gmail.com>
2026-09-19 20:22:27 -0400

    Refresh mobile HUD build cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hearthmere-nightfalltesting-v3';
+const CACHE='hearthmere-nightfalltesting-v4';
 const CORE=["./","./index.html","./app.js","./manifest.webmanifest"];
 const CDN=[
   "https://cdn.jsdelivr.net/npm/three@0.181.1/build/three.module.js",
